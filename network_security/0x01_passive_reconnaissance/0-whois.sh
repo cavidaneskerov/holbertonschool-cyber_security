c91663ddf2fa6bb1f844222af23e48d6e6482c12
holberton <cavidansgrv06@gmail.com>
2025-12-23 10:10:20 +0000

    task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^(Registrant|Admin|Tech) (Name|Organization|City|State\/Province|Postal Code|Country|Phone|Email)/{print $1 "," $2} /Street/{print $1 "," $2 " "} /(Phone|Fax) Ext/{print $1 ":,"} /^(Registrant|Admin|Tech) Fax:/{print $1 "," $2}'
+whois "$1" | awk -F': ' '/Registrant |Admin |Tech / && !/ID/ {k=$1; v=$2} /Street/ {v=v" "} /Phone Ext|Fax Ext/ {print k":, "} !/Ext/ && v!="" {print k", "v}' > "$1.csv"
