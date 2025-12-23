f507079f3fe1900b8473c3b5ec85edb674d83c9a
holberton <cavidansgrv06@gmail.com>
2025-12-23 10:07:05 +0000

    task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 whois "$1" | awk -F: '/^(Registrant|Admin|Tech) (Name|Organization|City|State\/Province|Postal Code|Country|Phone|Email)/{print $1 "," $2} /Street/{print $1 "," $2 " "} /(Phone|Fax) Ext/{print $1 ":,"} /^(Registrant|Admin|Tech) Fax:/{print $1 "," $2}'
-
