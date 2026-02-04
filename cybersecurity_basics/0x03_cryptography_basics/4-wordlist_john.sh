d1ddb0497e0dee4e75c894f9c988d0b70db9fc91
holberton <cavidansgrv06@gmail.com>
2026-02-04 04:16:48 -0500

    task

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt $1 > 4-password.txt
+john --input-encoding=UTF-8 --format=Raw-SHA256 --wordlist=/usr/share/wordlists/rockyou.txt $1 > 4-password.txt
