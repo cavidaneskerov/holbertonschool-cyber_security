e5f9dfc63abb898195a8c98a9a5dcd979416254f
cavidaneskerov <cavidansgrv06@gmail.com>
2026-04-18 07:40:19 -0400

    Update 2-accounts.sh

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Failed password for" | awk '{print $9}' | sort -u
+tail -n 1000 auth.log* | grep "Failed password for" | grep -v "invalid user" | awk '{print $9}' | sort | uniq -c | sort -nr | head -1 | awk '{print $2}'
