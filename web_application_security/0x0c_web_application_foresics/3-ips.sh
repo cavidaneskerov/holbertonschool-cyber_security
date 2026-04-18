2410acd31698b8460084ecb7b41d0c70365c584f
cavidaneskerov <cavidansgrv06@gmail.com>
2026-04-18 07:43:36 -0400

    Update 3-ips.sh

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "Accepted" auth.log | awk '{print $11}' | sort -u | wc -l
+grep -E 'sshd.*Accepted password for root' auth.log | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u | wc -l
